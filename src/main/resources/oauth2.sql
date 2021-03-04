48d7c6ea085efd82f00a3f6e6383c8ebf30f03aa
myou <myoueva@gmail.com>
2021-03-04 15:35:01 +0800

    增加sql文档

---
+++ src/main/resources/oauth2.sql
@@ -0,0 +1,73 @@
+-- spring security oauth2 持久化数据库结构
+-- used that use mysql
+create table oauth_client_details (
+  id BIGINT UNSIGNED auto_increment COMMENT "第三方ID",
+  name VARCHAR(64) not null COMMENT "注册名称",
+  phone VARCHAR(16) not null,
+  client_id VARCHAR(256),
+  resource_ids VARCHAR(256),
+  client_secret VARCHAR(256),
+  scope VARCHAR(256),
+  authorized_grant_types VARCHAR(256),
+  web_server_redirect_uri VARCHAR(256),
+  authorities VARCHAR(256),
+  access_token_validity INTEGER,
+  refresh_token_validity INTEGER,
+  additional_information VARCHAR(4096),
+  autoapprove VARCHAR(256),
+  platform_id BIGINT UNSIGNED,
+  PRIMARY KEY (id)
+);
+
+create table oauth_client_token (
+  token_id VARCHAR(256),
+  token BLOB,
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256)
+);
+
+create table oauth_access_token (
+  token_id VARCHAR(256),
+  token BLOB,
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256),
+  authentication BLOB,
+  refresh_token VARCHAR(256)
+);
+
+create table oauth_refresh_token (
+  token_id VARCHAR(256),
+  token BLOB,
+  authentication BLOB
+);
+
+create table oauth_code (
+  code VARCHAR(256), authentication BLOB
+);
+
+create table oauth_approvals (
+	userId VARCHAR(256),
+	clientId VARCHAR(256),
+	scope VARCHAR(256),
+	status VARCHAR(10),
+	expiresAt TIMESTAMP,
+	lastModifiedAt TIMESTAMP
+);
+
+
+-- customized oauth_client_details table
+create table ClientDetails (
+  appId VARCHAR(256) PRIMARY KEY,
+  resourceIds VARCHAR(256),
+  appSecret VARCHAR(256),
+  scope VARCHAR(256),
+  grantTypes VARCHAR(256),
+  redirectUrl VARCHAR(256),
+  authorities VARCHAR(256),
+  access_token_validity INTEGER,
+  refresh_token_validity INTEGER,
+  additionalInformation VARCHAR(4096),
+  autoApproveScopes VARCHAR(256)
+);
---
+++ src/main/resources/rbac.sql
@@ -0,0 +1,169 @@
+CREATE TABLE `organization` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COMMENT '组织名称',
+  `address` varchar(64) COMMENT '地址',
+  `legal` varchar(64) COMMENT '法人',
+  `description` varchar(256) DEFAULT NULL COMMENT '备注',
+  `phone` varchar(16) comment '联系电话',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='组织表';
+
+CREATE TABLE `department` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COMMENT '部门名称',
+  `level` int COMMENT '部门等级',
+  `pid` bigint UNSIGNED COMMENT '上级部门id',
+  `description` varchar(256) DEFAULT NULL COMMENT '备注',
+  `seq` varchar(16) comment '部门序号',
+  `org_id` bigint unsigned comment '所属组织',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='部门表';
+
+CREATE TABLE `user` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COMMENT '用户姓名',
+  `sex` TINYINT COMMENT '用户性别',
+  `age` int COMMENT '用户年龄',
+  `phone` varchar(32) COMMENT '用户联系电话',
+  `email` varchar(32) COMMENT '用户邮箱',
+  `birthday` varchar(16) comment '用户出生日期',
+  `account` varchar(32) comment '用户账号',
+  `password` varchar(128) comment '用户密码',
+  `description` varchar(256) comment '用户备注',
+  `status` int default 0 comment '账户登录状态 0:正常 1:登录失效(暂做扩展)',
+  `deleted` int default 0 comment '删除状态 0:未删除 1:已删除',
+  `locked` int default 0 comment '账户状态 0:正常 1:锁定',
+  `dpt_id` bigint unsigned comment '所属部门',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+CREATE TABLE `group` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COMMENT '用户组别名',
+  `description` varchar(256) comment '用户组备注',
+  `status` int default 0 comment '状态 0:正常 1:暂无法使用(暂做扩展)',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户组';
+
+CREATE TABLE `user_group` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` bigint unsigned comment '用户id',
+  `gid` bigint unsigned comment '用户组id',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户——用户组中间表';
+
+CREATE TABLE `role` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `code` varchar(16) COMMENT '角色编码',
+  `name` varchar(32) COMMENT '角色名称',
+  `status` int default 0 comment '状态 0:正常 1:暂无法使用(暂做扩展)',
+  `description` varchar(256) comment '用户备注',
+  `level` int COMMENT '角色等级',
+  `pid` bigint unsigned default 0 COMMENT '父角色id',
+  `seq` int comment '角色序号',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+CREATE TABLE `role_category` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `rid` bigint unsigned COMMENT '角色id',
+  `name` varchar(32) COMMENT '角色类别名称',
+  `status` int default 0 comment '状态 0:正常 1:暂无法使用(暂做扩展)',
+  `description` varchar(256) comment '用户备注',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色类别表';
+
+
+CREATE TABLE `user_role` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` bigint unsigned COMMENT 'user_id',
+  `rid` bigint unsigned COMMENT 'role_id',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色表';
+
+
+CREATE TABLE `group_role` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `gid` bigint unsigned COMMENT 'group_id',
+  `rid` bigint unsigned COMMENT 'role_id',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户组角色表';
+
+
+CREATE TABLE `explorer` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) COMMENT '资源名称',
+  `status` int default 0 comment '状态 0:正常 1:暂无法使用(暂做扩展)',
+  `description` varchar(256) comment '资源备注',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资源控制表';
+
+CREATE TABLE `menu` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `eid` bigint unsigned comment '资源id',
+  `name` varchar(32) COMMENT '菜单名称',
+  `icon` varchar(64) comment '菜单矢量图标或地址',
+  `status` int default 0 comment '状态 0:正常 1:暂无法使用(暂做扩展)',
+  `description` varchar(256) comment '菜单备注',
+  `href` varchar(64) comment '菜单连接地址',
+  `pid` bigint unsigned comment '父菜单id',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单表';
+
+CREATE TABLE `interface` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `eid` bigint unsigned  comment '资源id',
+  `name` varchar(32) COMMENT '接口名称',
+  `status` int default 0 comment '状态 0:正常 1:暂无法使用(暂做扩展)',
+  `description` varchar(256) comment '资源备注',
+  `service_name` varchar(64) comment '接口所属应用',
+  `created_by` varchar(16) NOT NULL comment '创建人',
+  `updated_by` varchar(16) NOT NULL comment '修改人',
+  `ctime` datetime default current_timestamp comment '创建时间',
+  `utime` datetime default current_timestamp on update current_timestamp comment '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='接口表';
