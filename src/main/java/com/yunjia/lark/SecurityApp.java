00da7e807f79ab4f93745247d1b8f3ba888dd4c9
gyli <ligy@yunjiacloud.com>
2021-03-04 16:57:43 +0800

    数据库配置

--- src/main/java/com/yunjia/lark/SecurityApp.java
+++ src/main/java/com/yunjia/lark/SecurityApp.java
@@ -1,5 +1,6 @@
 package com.yunjia.lark;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 认证授权中心启动类
  */
 @SpringBootApplication
+@MapperScan({"com.yunjia.lark.mapper"})
 public class SecurityApp {
     public static void main( String[] args ) {
         SpringApplication.run(SecurityApp.class, args);
