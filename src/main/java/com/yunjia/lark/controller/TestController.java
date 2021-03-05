53f9e6a7224839c9a5b829f6bc5a758aa6f69497
gyli <ligy@yunjiacloud.com>
2021-03-05 09:32:05 +0800

    删除测试controller

--- src/main/java/com/yunjia/lark/controller/TestController.java
+++ src/main/java/com/yunjia/lark/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.yunjia.lark.controller;
 
+import com.yunjia.lark.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class TestController {
+
+    @Autowired
+    private SysUserService sysUserService;
 
     @GetMapping(value = "/test")
     public String test() {
