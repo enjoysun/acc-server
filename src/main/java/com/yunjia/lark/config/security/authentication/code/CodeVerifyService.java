547d93d609486946b9eb43e5dbf4142852684ed5
myou <myoueva@gmail.com>
2021-04-13 11:21:57 +0800

    增加验证码抽象默认实现接口

---
+++ src/main/java/com/yunjia/lark/config/security/authentication/code/CodeVerifyService.java
@@ -0,0 +1,30 @@
+package com.yunjia.lark.config.security.authentication.code;
+
+import com.yunjia.lark.util.EncryptorsKey;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
+
+
+public interface CodeVerifyService {
+    default boolean verifyCode(HttpServletRequest request, StringRedisTemplate redisTemplate) {
+        String key = request.getHeader("username");
+        String clientCode = request.getHeader("code");
+        if (!StringUtils.isEmpty(key) && null != redisTemplate) {
+            if (redisTemplate.hasKey(key)) {
+                String code = redisTemplate.opsForValue().get(EncryptorsKey.hashString(key));
+                if (StringUtils.equals(code, clientCode))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    default boolean setVerifyCodeExpire(Authentication authentication, String code, StringRedisTemplate redisTemplate) {
+        redisTemplate.opsForValue().set(EncryptorsKey.hashString(authentication.getName()), code, 60000, TimeUnit.MILLISECONDS);
+        return true;
+    }
+}
